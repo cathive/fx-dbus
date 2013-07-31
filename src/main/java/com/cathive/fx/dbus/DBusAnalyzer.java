7139c9a8f7060fd569201f46fbebbb4f7d4efe8c
Benjamin P. Jung <headcr4sh@gmail.com>
2013-07-31 18:16:29 +0200

    Add skeleton for DBus analyzer JavaFX app.

---
+++ src/main/java/com/cathive/fx/dbus/DBusAnalyzer.java
@@ -0,0 +1,15 @@
+package com.cathive.fx.dbus;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Benjamin P. Jung
+ */
+public class DBusAnalyzer extends Application {
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+    }
+
+}
